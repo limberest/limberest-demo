b7617cd5def6cd5adf5c5f1b560fea2408fab0d5
Donald Oakes <donald@oakesville.io>
2017-05-30 20:03:09 -0600

    docs

--- src/io/limberest/demo/api/Definition.java
+++ src/io/limberest/demo/api/Definition.java
@@ -5,7 +5,7 @@ import io.swagger.annotations.License;
 import io.swagger.annotations.SwaggerDefinition;
 
 @SwaggerDefinition(
-    info=@Info(title="Movies API", version="1.0.01", license=@License(name="Apache 2.0")),
+    info=@Info(title="Movies API", version="1.0", license=@License(name="Apache 2.0")),
     host="limberest.io",
     schemes={SwaggerDefinition.Scheme.HTTP,SwaggerDefinition.Scheme.HTTPS},
     basePath="/demo", consumes="application/json", produces="application/json")
