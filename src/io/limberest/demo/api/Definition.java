5902b225491c4d53f2c2c2828720b7d66dc764da
Donald Oakes <donald@oakesville.io>
2017-06-19 19:37:53 -0600

    Wire for HTTPS so swagger-ui Try It works.

--- src/io/limberest/demo/api/Definition.java
+++ src/io/limberest/demo/api/Definition.java
@@ -6,7 +6,7 @@ import io.swagger.annotations.SwaggerDefinition;
 
 @SwaggerDefinition(
     info=@Info(title="Movies API", version="1.0", license=@License(name="Apache 2.0")),
-    schemes={SwaggerDefinition.Scheme.HTTP,SwaggerDefinition.Scheme.HTTPS},
+    schemes={SwaggerDefinition.Scheme.HTTPS},
     basePath="/demo", consumes="application/json", produces="application/json")
 public class Definition {
 
