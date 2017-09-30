1544dcc7e2c4330fffbeba019b95afd5f79c5cc0
Donald Oakes <donald@oakesville.io>
2017-09-30 12:46:12 -0600

    test cases

--- test/cases/movies-query.js
+++ test/cases/movies-query.js
@@ -1,9 +1,23 @@
 'use strict';
 
-// example limberest-js test case
-var limberest = require('limberest');
+const limberest = require('limberest');
+const demo = require('../lib/limberest-demo');
 
-var env = limberest.env('../limberest.io.env');
+var options = demo.getOptions();
 
-var group =  limberest.group('../limberest-demo.postman');
-var resp = limberest.run(group.api-docs);
+// programmatically run a single test against limberest.io
+limberest.loadValues(options.location + '/limberest.io.values', function(err, vals) {
+  if (err)
+    throw err;
+  var values = Object.assign({}, vals);
+  limberest.loadGroup(options.location + '/movies-api.postman', function(err, group) {
+    if (err)
+      throw err;
+    var test = group.getTest('GET', 'movies?{query}');
+    // overriding some values
+    values.query = 'year=1935&rating=5';
+    test.run(options, values, (error, response) => {
+      test.verify(values);
+    });
+  });
+});
---
+++ test/lib/limberest-demo.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const limberest = require('limberest');
+
+function LimberestDemo() {
+}
+
+// Returns options as appropriate for browser vs local. 
+LimberestDemo.prototype.getOptions = function() {
+  var testsLoc = '..';
+  if (typeof window !== 'undefined') {
+    // in browser
+    testsLoc = 'https://raw.githubusercontent.com/limberest/limberest-demo/master/test';
+  }
+  return {
+    location: testsLoc,
+    expectedResultLocation: testsLoc + '/results/expected',
+    resultLocation: '../../limberest-demo/test/results/actual',
+    debug: true,
+    responseHeaders: ['content-type']
+  }
+};
+
+module.exports = new LimberestDemo();
