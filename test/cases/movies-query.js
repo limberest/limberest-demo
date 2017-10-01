5921e4abeccaad712474d87cb1c6de765d3aa24e
Donald Oakes <donald@oakesville.io>
2017-10-01 10:45:07 -0600

    API updates.

--- test/cases/movies-query.js
+++ test/cases/movies-query.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const limberest = require('limberest');
+const limberest = require('../../../limberest-js/lib/limberest');
 const demo = require('../lib/limberest-demo');
 
 var options = demo.getOptions();
--- test/cases/temp.js
+++ test/cases/temp.js
@@ -3,6 +3,7 @@
 const limberest = require('limberest');
 const demo = require('../lib/limberest-demo');
 
+console.log("HERE I AM");
 
 console.log("limberest: " + limberest);
 console.log("demo: " + demo);
