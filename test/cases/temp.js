03ffa47aaf3bd6f8b3710eb9b591645da5abeec5
Donald Oakes <donald@oakesville.io>
2017-10-06 19:07:53 -0600

    remove temp case

--- test/cases/temp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const limberest = require('limberest');
-const demo = require('../lib/limberest-demo');
-
-console.log("HERE I AM");
-
-console.log("limberest: " + limberest);
-console.log("demo: " + demo);
