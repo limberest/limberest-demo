f6d42c83206eba865906614b22002dc14cd552d1
Donald Oakes <donald@oakesville.io>
2017-10-09 12:40:35 -0600

    Case execution.

--- test/cases/movie-crud.js
+++ test/cases/movie-crud.js
@@ -4,19 +4,62 @@ const limberest = require('../../../limberest-js/lib/limberest');
 const demo = require('../lib/limberest-demo');
 
 var options = demo.getOptions();
-
-var movieId = '435b30ad';
+var valuesFiles = ['global.values', 'limberest.io.values'];
 
 const Case = limberest.Case;
 var testCase = new Case('movie-crud', options);
+var logger = testCase.logger;
 
-demo.cleanupMovie(Object.assign({}, options, {debug: false}), movieId, (err, response) => {
-  if (err) {
-    testCase.logger.error('Cleanup error: ' + err);
-  }
-  else {
-    testCase.logger.info('Cleanup response status: ' + response.status.code);
-    testCase.authHeader = demo.getAuthHeader();
-    
-  }
+// programmatically run an orchestrated sequence of tests
+limberest.loadValues(options, valuesFiles, (err, vals) => {
+  if (err)
+    logger.error(err);
+
+  var values = Object.assign({}, vals);
+
+  demo.cleanupMovie(values, (err, response) => {
+    if (err) {
+      logger.error('Cleanup error: ' + err);
+    }
+    else {
+      logger.info('Cleanup completed for: ' + values.id);
+      testCase.authHeader = demo.getAuthHeader();
+      
+      limberest.loadGroup(options.location + '/movies-api.postman', (err, group) => {
+        if (err) {
+          logger.error(err);
+        }
+        else {
+          // create a movie
+          var post = group.getTest('POST', 'movies');
+          testCase.run(post, values, (err, response) => {
+            if (!err) {
+              // update it
+              var put = group.getTest('PUT', 'movies/{id}');
+              testCase.run(put, values, (err, response) => {
+                if (!err) {
+                  // confirm update
+                  var get = group.getTest('GET', 'movies/{id}');
+                  testCase.run(get, values, (err, response) => {
+                    if (!err) {
+                      // delete it
+                      var del = group.getTest('DELETE', 'movies/{id}');
+                      testCase.run(del, values, (err, response) => {
+                        // confirm delete
+                        testCase.run(get, values, (err, response) => {
+                          // verify results
+                          var res = testCase.verify(values, (err, result) => {
+                          });
+                        });
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });    
+    }
+  });
 });
--- test/lib/limberest-demo.js
+++ test/lib/limberest-demo.js
@@ -21,7 +21,8 @@ LimberestDemo.prototype.getOptions = function() {
     expectedResultLocation: testsLoc + '/results/expected',
     resultLocation: '../results/actual',
     debug: true,
-    responseHeaders: ['content-type']
+    responseHeaders: ['content-type'],
+    retainResult: true
   }
 };
 
@@ -48,41 +49,33 @@ LimberestDemo.prototype.getAuthHeader = function() {
   }
 };
 
-LimberestDemo.prototype.cleanupMovie = function(options, id, callback) {
+LimberestDemo.prototype.cleanupMovie = function(values, callback) {
   try {
+    var options = Object.assign({}, this.getOptions(), {retainResult: false});
     var authHeader = this.getAuthHeader();
     // programmatically run a single test against limberest.io
-    limberest.loadValues(options.location + '/limberest.io.values', (err, vals) => {
+    limberest.loadGroup(options.location + '/movies-api.postman', (err, group) => {
       if (err) {
         callback(err);
       }
       else {
-        var values = Object.assign({}, vals);
-        limberest.loadGroup(options.location + '/movies-api.postman', (err, group) => {
+        var test = group.getTest('DELETE', 'movies/{id}');
+        if (!test.request.headers)
+          test.request.headers = {};
+        if (authHeader)
+          test.request.headers.Authorization = authHeader;
+        test.run(options, values, (err, response) => {
           if (err) {
             callback(err);
           }
           else {
-            var test = group.getTest('DELETE', 'movies/{id}');
-            if (!test.request.headers)
-              test.request.headers = {};
-            if (authHeader)
-              test.request.headers.Authorization = authHeader;
-            values.id = id;
-            test.run(options, values, (err, response) => {
-              if (err) {
-                callback(err);
-              }
-              else {
-                if (response.status.code === 200 || response.status.code === 404) {
-                  // success if deleted or not found
-                  callback(null, response);
-                }
-                else {
-                  callback(new Error(response.status.code + ': ' + response.status.message), response);
-                }
-              }
-            });
+            if (response.status.code === 200 || response.status.code === 404) {
+              // success if deleted or not found
+              callback(null, response);
+            }
+            else {
+              callback(new Error(response.status.code + ': ' + response.status.message), response);
+            }
           }
         });
       }
