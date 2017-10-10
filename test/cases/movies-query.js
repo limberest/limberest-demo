bccac995df1b71470bd666d62a8c04f17b605d39
Donald Oakes <donald@oakesville.io>
2017-10-10 11:35:12 -0600

    more cases

--- test/cases/movies-query.js
+++ test/cases/movies-query.js
@@ -4,20 +4,44 @@ const limberest = require('../../../limberest-js/lib/limberest');
 const demo = require('../lib/limberest-demo');
 
 var options = demo.getOptions();
+var valuesFiles = ['global.values', 'limberest.io.values'];
 
-// programmatically run a single test against limberest.io
-limberest.loadValues(options.location + '/limberest.io.values', (err, vals) => {
-  if (err)
-    throw err;
+var caseName = 'movies-query';
+var logger = demo.getLogger('movies-api', caseName);
+
+// programmatically run an orchestrated sequence of tests
+limberest.loadValues(options, valuesFiles, (err, vals) => {
+  if (err) {
+    logger.error(err);
+    return;
+  }
+  
   var values = Object.assign({}, vals);
+  
   limberest.loadGroup(options.location + '/movies-api.postman', (err, group) => {
-    if (err)
-      throw err;
-    var test = group.getTest('GET', 'movies?{query}');
-    // overriding some values
-    values.query = 'year=1935&rating=5';
-    test.run(options, values, (error, response) => {
-      test.verify(values);
-    });
+    if (err) {
+      logger.error(err);
+    }
+    else {
+      // start a new case
+      var testCase = new limberest.Case(caseName, options);
+      testCase.authHeader = demo.getAuthHeader();
+
+      // create a movie
+      var post = group.getTest('GET', 'movies?{query}');
+      // change query
+      values.query='year=1931&rating=>4';
+      testCase.run(post, values, (err, response) => {
+        if (!err) {
+          // verify results
+          var res = testCase.verify(values, (err, result) => {
+            if (err)
+              logger.error(err);
+            if (demo.getCallback())
+              demo.getCallback()(err, result, values);
+          });
+        }
+      });
+    }
   });
 });
