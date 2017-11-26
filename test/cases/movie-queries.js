e8c7142d250316aa151bb8958c37754358838af9
Donald Oakes <donald@oakesville.io>
2017-11-25 19:08:45 -0700

    cleanup

---
+++ test/cases/movie-queries.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// const limberest = require('limberest');
+const limberest = require('../../../limberest-js/lib/limberest');
+const demo = require('../lib/limberest-demo');
+
+// TODO
